e4cf678d9c79f28b6e5efc5a306b505a803678bf
Kanin-McGuire <kamc9940@colorado.edu>
2024-04-22 20:56:59 -0600

    update make

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -4,14 +4,19 @@ case "$1" in
     start)
         echo "Starting aesdsocket"
         start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+        echo "Starting simulate.py"
+        python simulate.py &
         ;;
     stop)
         echo "Stopping aesdsocket"
         start-stop-daemon -K -n aesdsocket -15
+        echo "Stopping simulate.py"
+        pkill -f "python simulate.py"
         ;;
     *)
         echo "Usage: $0 {start|stop}"
-    exit 1
+        exit 1
+        ;;
 esac
 
 exit 0
