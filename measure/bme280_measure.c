fc955e5e274ed494e5eb4e880b5e8441db8a1b3d
thra6614 <thomas.ramirez@colorado.edu>
2024-05-01 11:50:10 -0600

    try to fix looping

--- measure/bme280_measure.c
+++ measure/bme280_measure.c
@@ -12,20 +12,32 @@
 
 int main() {
     int retval = 0;
-    while (1) {
-        int bme280_dev_fd;
-        char temp_buffer[LONG_SIGNED_INT_NUM] = "123";
-        uint8_t num_bytes_read = 0;
-        
-        long signed int temperaturef, pressf;
-        char *endptr;
-        // Open I2C device file
-        bme280_dev_fd = open(BME280_DEV, O_CREAT | O_RDWR, 0744);
-        if (bme280_dev_fd < 0)
-        {
-            perror("Failed to open I2C device file");
-            return -1;
+    // Open connection to the database
+
+    
+    int bme280_dev_fd;
+    char temp_buffer[LONG_SIGNED_INT_NUM] = "123";
+    uint8_t num_bytes_read = 0;
+    
+    long signed int temperaturef, pressf;
+    char *endptr;
+    // Open I2C device file
+    bme280_dev_fd = open(BME280_DEV, O_CREAT | O_RDWR, 0744);
+    if (bme280_dev_fd < 0)
+    {
+        perror("Failed to open I2C device file");
+        return -1;
+    }
+    if (sqlite3_open("finalProject.db", &db) != SQLITE_OK) {
+        // If the database doesn't exist, create it
+        if (sqlite3_open("finalProject.db", &db) == SQLITE_OK) {
+            // Perform any initial setup here if needed
+        } else {
+            fprintf(stderr, "Failed to open/create database: %s\n", sqlite3_errmsg(db));
+            return 1;
         }
+    }  
+    while (1) {          
         // Read temperature from BME280 sensor
         num_bytes_read = read(bme280_dev_fd, temp_buffer, LONG_SIGNED_INT_NUM);
         if (num_bytes_read < 0)
@@ -64,16 +76,7 @@ int main() {
         sqlite3 *db;
         char *errMsg = 0;
 
-        // Open connection to the database
-        if (sqlite3_open("finalProject.db", &db) != SQLITE_OK) {
-            // If the database doesn't exist, create it
-            if (sqlite3_open("finalProject.db", &db) == SQLITE_OK) {
-                // Perform any initial setup here if needed
-            } else {
-                fprintf(stderr, "Failed to open/create database: %s\n", sqlite3_errmsg(db));
-                return 1;
-            }
-        }
+        
 
         // SQL statement for table creation if it doesn't exist
         const char *createTableSQL = "CREATE TABLE IF NOT EXISTS sensor_data ("
@@ -124,14 +127,16 @@ int main() {
 
         // Finalize the statement and close the database connection
         sqlite3_finalize(stmt);
-        sqlite3_close(db);
 
-    close_and_exit:
-        // Close I2C device file
-        close(bme280_dev_fd);
+
+
 
         // Delay for 5 seconds
         sleep(5);
     }
+    close_and_exit:
+        // Close I2C device file
+        sqlite3_close(db);
+        close(bme280_dev_fd);    
     return retval;
 }
